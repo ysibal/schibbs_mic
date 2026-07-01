ce29e893c91f7afce53afdf512b7f8c7e5dff4e7
Yigit Sibal <sibal.yigit@gmail.com>
2026-07-01 16:57:34 +0300

    add in-room color theme selector

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "schibbs-mic-shell-v15";
+const CACHE_NAME = "schibbs-mic-shell-v16";
 const SHELL_ASSETS = [
   "/",
   "/index.html",
-  "/styles.css?v=11",
+  "/styles.css?v=12",
   "/app.js?v=12",
   "/manifest.webmanifest",
   "/icon.svg"
